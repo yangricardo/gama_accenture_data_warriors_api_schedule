ca91dad1e96aa13d55462db00ef4a555ce4a394a
Yang Ricardo Barcellos Miranda <yang_ricardo@hotmail.com>
2021-03-11 20:42:53 -0300

    postgress class documentation

--- postgres.py
+++ postgres.py
@@ -2,38 +2,82 @@ import psycopg2
 
 
 class Postgres():
-    def __init__(self, database='postgres', user='DataWarriorsAdmin@data-warriors-postgresql', password='DataWarriorsPassword!', host='data-warriors-postgresql.postgres.database.azure.com', port='5432'):
+    '''
+    Returns a object prepared to connect with an Postgres Database
+
+    INPUT:
+        database - string that represents the database name
+        user - string that represents an user from a given database
+        password - string that authenticates the connection with a given database
+        host - string that represents the host to access a given database
+        port - string that represents the port to access a given database
+        sslmode - string that ssl connection mode based on psycopg2
+
+    Author: Yang Miranda
+    '''
+
+    def __init__(self, database='postgres', user='DataWarriorsAdmin@data-warriors-postgresql', password='DataWarriorsPassword!', host='data-warriors-postgresql.postgres.database.azure.com', port='5432', sslmode='require'):
         self.connection = psycopg2.connect(
-            host=host, database=database, user=user, password=password, port=port, sslmode='require')
+            host=host, database=database, user=user, password=password, port=port, sslmode=sslmode)
         self.new_cursor()
 
     def get_connection(self):
+        '''
+        Returns the connection object
+        '''
         return self.connection
 
     def new_cursor(self):
+        '''
+        Returns the cursor object from connection
+        '''
         self.cursor = self.connection.cursor()
         return self.cursor
 
     def commit(self):
+        '''
+        commits the transaction to a connected database
+        '''
         self.connection.commit()
 
     def rollback(self):
+        '''
+        rollbacks the transaction in case of failure
+        '''
         self.connection.rollback()
 
     def fetchall(self):
+        '''
+        fetch all values resulted from a query execution
+        '''
         return self.cursor.fetchall()
 
     def execute(self, query):
+        '''
+        execute and commit a transaction query
+        '''
         self.cursor.execute(query)
         self.commit()
 
     def search(self, query):
+        '''
+        execute a search query, like an select
+        '''
         self.new_cursor()
         self.cursor.execute(query)
         self.commit()
         return self.fetchall()
 
     def insertMany(self, query, data_list):
+        '''
+        insert a tuple list based on a insert query
+            INPUT:
+                - query - string that represents a insert query like
+                - data_list - tuple list that will be inserted by a query
+            OUTPUT:
+                - success - print('Execução da transação conclída com sucesso:', query) 
+                - failure - print('Transação falhou:', error)
+        '''
         try:
             self.new_cursor()
             self.cursor.executemany(query, data_list)
@@ -44,6 +88,9 @@ class Postgres():
             print('Transação falhou:', error)
 
     def closeConnection(self):
+        '''
+        close connection of object
+        '''
         self.connection.close()
 
 

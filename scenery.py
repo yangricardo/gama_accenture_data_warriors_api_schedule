398507e81e3e56dca662d766e5f20fb0f338f86f
Yang Ricardo Barcellos Miranda <yang_ricardo@hotmail.com>
2021-03-11 22:32:03 -0300

    scenery - menu

--- scenery.py
+++ scenery.py
@@ -1,7 +1,5 @@
 import pandas as pd
 from postgres import Postgres
-database = Postgres()
-database.search()
 
 
 class Scenery (object):
@@ -14,7 +12,7 @@ class Scenery (object):
         - total_cases
         - total_deaths
 
-    Authors: Gabriel Rangel and Yang Miranda
+    Authors: Gabriel Rangel
     '''
 
     def __init__(self, database=Postgres()):
@@ -87,3 +85,53 @@ class Scenery (object):
             response, columns=['País', 'Total de Mortes', 'Data de Atualização'])
         title = 'Total acumulado de mortes por Covid-19 registradas até a data mais recente: '
         return title, total_deaths_df
+
+
+if __name__ == '__main__':
+    def handle_user_option():
+        option = 0
+
+        print("""
+Panorama DataWarriors - Gama Accenture 2021
+Digite a opção que deseja executar:
+    1. Panorama diário de quantidade de casos confirmados de COVID-19 dos 10 países do mundo com mais casos.
+    2. Panorama diário de quantidade de mortes de COVID-19 dos 10 países do mundo com mais mortes.
+    3. Total de mortes por COVID-19 dos 10 países do mundo com mais casos.
+    4. Total de casos confirmados por COVID-19 dos 10 países do mundo com maiores números.
+    5. Sair.
+        """)
+
+        while option < 1 or option > 5:
+            try:
+                option = int(input("\nDigite uma opção de 1 a 5: "))
+            except:
+                print("\nOpção inválida. Digite novamente\n")
+
+        return option
+
+    def print_response(scenery_response_tuple):
+        print(scenery_response_tuple[0])
+        print(scenery_response_tuple[1])
+
+    option = handle_user_option()
+    scenery = Scenery()
+
+    while option >= 1 and option <= 5:
+        if option == 1:
+            print_response(scenery.daily_cases())
+
+        elif option == 2:
+            print_response(scenery.daily_deaths())
+
+        elif option == 3:
+            print_response(scenery.total_deaths())
+
+        elif option == 4:
+            print_response(scenery.total_cases())
+
+        else:
+            print("\nEncerrando Panorama DataWarriors - Gama Accenture 2021 \n")
+            break
+
+        if option != 5:
+            option = handle_user_option()

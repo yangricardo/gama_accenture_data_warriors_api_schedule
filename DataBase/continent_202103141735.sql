cec518e91144b9789369e61e949b89e17cfab7e4
Gabriel Souza Rangel <gsrangel6@gmail.com>
2021-03-18 12:32:22 -0300

    Add database set

---
+++ DataBase/continent_202103141735.sql
@@ -0,0 +1,8 @@
+INSERT INTO public.continent (continentcode,continentname) VALUES
+	 ('AS','Asia'),
+	 ('EU','Europe'),
+	 ('AN','Antarctica'),
+	 ('AF','Africa'),
+	 ('OC','Oceania'),
+	 ('NA','North America'),
+	 ('SA','South America');
